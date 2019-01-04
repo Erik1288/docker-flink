24eb1cfdf89b1e6a987bc4ed3d8cd0e80452d039
Eric <erikfei1288@163.com>
2019-01-04 11:16:28 +0800

    Update docker-entrypoint.sh
    support k8s deployment

--- 1.7/scala_2.11-alpine/docker-entrypoint.sh
+++ 1.7/scala_2.11-alpine/docker-entrypoint.sh
@@ -47,7 +47,6 @@ elif [ "$1" = "taskmanager" ]; then
     TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
 
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
-    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
     echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
     echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
